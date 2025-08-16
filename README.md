0c1f573bf8e776c8e3de5690934e5ed1f6bf9953
aktk-lab <worldflower333@gmail.com>
2025-08-16 16:19:43 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-dacwkgap
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/aktk-lab/vitejs-vite-dacwkgap)
---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'ringshogi-v1';
+const ASSETS = [
+  '/', '/index.html', '/manifest.webmanifest',
+  '/sw.js', '/icons/icon-192.png', '/icons/icon-512.png'
+];
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));
+});
+self.addEventListener('fetch', e=>{
+  e.respondWith(caches.match(e.request).then(res=>res||fetch(e.request)));
+});
+self.addEventListener('message', (e) => {
+  if (e.data && e.data.type === 'SKIP_WAITING') self.skipWaiting();
+});
